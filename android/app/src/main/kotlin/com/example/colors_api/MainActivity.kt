c64553e6ae000e6626ae0ebf25b03331a145524f
Sagar Dhadke <peaceunique2003@gmail.com>
2024-09-24 19:16:40 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/colors_api/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.colors_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/ColorsApi.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyColorsApi extends StatefulWidget {
+  const MyColorsApi({super.key});
+
+  @override
+  State<MyColorsApi> createState() => _MyColorsApiState();
+}
+
+class _MyColorsApiState extends State<MyColorsApi> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
