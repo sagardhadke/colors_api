aa859d90102e641e7ebc3d8d48dd4f9ce2392b89
Sagar Dhadke <peaceunique2003@gmail.com>
2024-09-24 19:33:54 +0530

    Added dummy design

--- lib/ColorsApi.dart
+++ lib/ColorsApi.dart
@@ -8,8 +8,56 @@ class MyColorsApi extends StatefulWidget {
 }
 
 class _MyColorsApiState extends State<MyColorsApi> {
+
+
+  void getColorsDetails()async{
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Colors API",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 100,
+              width: double.infinity,
+              color: Colors.white,
+              child: Row(
+                children: [
+                  Image.network(
+                      height: 75,
+                      width: 75,
+                      "https://www.colourlovers.com/img/9EAB65/100/100/greygreen.png"),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Center(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Title : "),
+                          Text("DateCreated : "),
+                          Text("Hex : "),
+                          Text("Username : "),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
